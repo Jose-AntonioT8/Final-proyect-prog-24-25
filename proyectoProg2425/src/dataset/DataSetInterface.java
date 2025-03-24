7f32ce5eb0c67c42162f65378ea9af6ef99b093e
José Antonio Domínguez gonzález <joseantdominguezgonzalez@gmail.com>
2025-03-24 09:34:45 +0100

    Delete proyectoProg2425 directory

--- proyectoProg2425/src/dataset/DataSetInterface.java
+++
@@ -1,7 +0,0 @@
-package dataset;
-
-public interface DataSetInterface {
-    public void importFromCSV(String file) throws Exception;
-    public void exportToCSV(String file) throws Exception;
-    
-}
